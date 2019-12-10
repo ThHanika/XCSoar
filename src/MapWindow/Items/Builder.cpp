ed5d02eaa1dcf79aca136d5e689cec0ca1a1172a
Thomas Hanika <thomas@hanika-co.de>
2019-12-10 22:34:55 +0100

    format von master übernommen

--- src/MapWindow/Items/Builder.cpp
+++ src/MapWindow/Items/Builder.cpp
@@ -1,25 +1,25 @@
 /*
- Copyright_License {
+Copyright_License {
 
- XCSoar Glide Computer - http://www.xcsoar.org/
- Copyright (C) 2000-2016 The XCSoar Project
- A detailed list of copyright holders can be found in the file "AUTHORS".
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2016 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- }
- */
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
 
 #include "Builder.hpp"
 #include "MapItem.hpp"
@@ -36,7 +36,9 @@
 #include "NMEA/Info.hpp"
 #include "Terrain/RasterTerrain.hpp"
 
-class WaypointListBuilderVisitor: public WaypointVisitor {
+class WaypointListBuilderVisitor:
+  public WaypointVisitor
+{
   MapItemList &list;
 
   /*
@@ -61,9 +63,7 @@ public:
   {
   }
 
-  void
-  Visit(const WaypointPtr & waypointptr) override
-  {
+  void Visit(const WaypointPtr &waypointptr) override {
     if (location.IsValid()) {
       /*
        * a valid location allows calculating the distance
@@ -134,16 +134,16 @@ MapItemListBuilder::AddLocation(const NMEAInfo &basic,
 
   double elevation = LocationMapItem::UNKNOWN_ELEVATION;
   if (terrain != nullptr)
-    elevation = terrain->GetTerrainHeight(location).ToDouble(
-        LocationMapItem::UNKNOWN_ELEVATION);
+    elevation = terrain->GetTerrainHeight(location)
+      .ToDouble(LocationMapItem::UNKNOWN_ELEVATION);
 
   list.append(new LocationMapItem(vector, elevation));
 }
 
 void
 MapItemListBuilder::AddArrivalAltitudes(
-    const ProtectedRoutePlanner &route_planner, const RasterTerrain *terrain,
-    double safety_height)
+    const ProtectedRoutePlanner &route_planner,
+    const RasterTerrain *terrain, double safety_height)
 {
   if (list.full())
     return;
@@ -151,8 +151,8 @@ MapItemListBuilder::AddArrivalAltitudes(
   // Calculate terrain elevation if possible
   double elevation = LocationMapItem::UNKNOWN_ELEVATION;
   if (terrain != nullptr)
-    elevation = terrain->GetTerrainHeight(location).ToDouble(
-        LocationMapItem::UNKNOWN_ELEVATION);
+    elevation = terrain->GetTerrainHeight(location)
+      .ToDouble(LocationMapItem::UNKNOWN_ELEVATION);
 
   // Calculate target altitude
   double target_elevation = 0;
@@ -209,8 +209,7 @@ MapItemListBuilder::AddTaskOZs(const ProtectedTaskManager &task)
       continue;
 
     const ObservationZonePoint &oz = task_point.GetObservationZone();
-    list.append(
-        new TaskOZMapItem(i, oz, task_point.GetType(),
-                          task_point.GetWaypointPtr()));
+    list.append(new TaskOZMapItem(i, oz, task_point.GetType(),
+                                  task_point.GetWaypointPtr()));
   }
 }
